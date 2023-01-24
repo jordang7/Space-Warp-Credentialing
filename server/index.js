a6f5a1f100873f4e88347e6097891950fc7a3b7d
jordang7 <77jgreenberg77@gmail.com>
2023-01-24 13:39:24 -0500

    basic routes getting data from filrep API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
-const express = require("express")
+import * as dotenv from "dotenv"
+dotenv.config()
+import express from "express"
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
+import fetch from "node-fetch"
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get("/", async (req, res) => {
+    try {
+        const response = await fetch("https://api.filrep.io/api/v1/miners", {
+            method: "GET",
+        })
+        const minerArray = await response.json()
+        res.send(minerArray)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+    }
+})
+
+app.post("/mintCredential", async (req, res) => {
+    const { minerId } = req.body
+    try {
+        const response = await fetch("https://api.filrep.io/api/v1/miners", {
+            method: "GET",
+        })
+        const minerArray = await response.json()
+        const ourMiner = minerArray.filter((c) => c.id === minerId)
+        res.send(ourMiner.score)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+    }
 })
 
 app.listen(port, () => {
